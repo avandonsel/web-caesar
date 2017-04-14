6c92c81ad847ba863091d32869e97d8fa2393988
avandonsel <amy@pikazoapp.com>
2017-04-13 19:22:22 -0500

    Finished project.

--- main.py
+++ main.py
@@ -16,28 +16,36 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    rotation_label = "<label>Rotate by:</lable>"
+    rotation_input = "<input type='number' name='rotation'/>"
+
+    message_label = "<label>Type a message!</lable>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+    submit = "<input type='submit'/>"
+
+    form = ("<form method='post'>" +
+            rotation_label + rotation_input + "<br>" +
+            message_label+ textarea + "<br>" + submit + "</form>")
+
+    header = "<h2>Web Caesar</h2>"
+
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        rotation_label = "<label>Rotate by:</lable>"
-        rotation_input = "<input type='number' name='rotation'/>"
-
-        message_label = "<label>Type a message!</lable>"
-        textarea = "<textarea name='message'>""</textarea>"
-        submit = "<input type='submit'/>"
-
-        form = ("<form method='post'>" +
-                rotation_label + rotation_input + "<br>" +
-                message_label+ textarea + "<br>" + submit + "</form>")
-
-        header = "<h2>Web Caesar</h2>"
-        self.response.write(header + form)
+        content = build_page("")
+        self.response.write(content)
 
     def post(self):
         message = self.request.get("message")
-        rotation = self.request.get("rotation")
+        rotation = int(self.request.get("rotation"))
         encrypted_message = caesar.encrypt(message, rotation)
-        self.response.write("Secret message: " + encrypted_message)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
